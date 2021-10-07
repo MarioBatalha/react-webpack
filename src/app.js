39ac1e6bd7c69bad04c23d0eb0330be2994d5a52
MarioBatalha <mariobatalha9@gmail.com>
2021-10-07 14:12:50 +0100

    add react and react-dom

---
+++ src/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var Title = React.createClass({
+    render: function() { 
+        return React.createElement('h1', null, 'Title');
+    }
+});
+
+module.exports = Title;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+'use strict';
+
+var Title = require('./app');
+
+ReactDOM.render(
+    React.createElement(Title),
+    document.querySelector('[data.js="app"]')
+);
